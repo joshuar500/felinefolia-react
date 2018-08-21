e615398bffd3fcd72eac458aa367a12af0a784f7
Josh Rincon <memonkey@gmail.com>
2018-08-20 20:44:28 -0700

    Update API config

--- src/api/request.js
+++ src/api/request.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
 export const request = axios.create({
-  baseURL: 'http://felinefolia.local:8000',
+  baseURL: 'http://api.felinefolia.com:8000',
   timeout: 1000
 });
